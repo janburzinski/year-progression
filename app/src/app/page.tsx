c40004ed52196ab3d5b807ac142d07afc752e0f1
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2025-03-06 07:33:02 +0100

    moved components folder outside of the src/app folder #gg

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Calendar } from "./components/Calendar";
+import { Calendar } from "../../components/Calendar";
 
 export default function Home() {
   return (
